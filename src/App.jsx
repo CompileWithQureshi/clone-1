e28af8f0fcc96028a14faf172bd826f6ae4690a9
Sajid <contactme.qureshi@gmail.com>
2023-09-27 09:41:10 -0700

    working on nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+// 1280 x 800 width
+
+import Nav from "./component/nav";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <section
+        className=" md:max-w-full md:h-full 
+      "
+      >
+        <Container />
+      </section>
+    </>
+  );
 }
 
 export default App;
+
+const Container = () => {
+  return (
+    <>
+      <main>
+        <div className=" max-w-5xl h-[800px] border border-black  rounded-xl grid    m-9 mx-auto ">
+          <Nav />
+        </div>
+      </main>
+    </>
+  );
+};
---
+++ src/component/nav.jsx
@@ -0,0 +1,25 @@
+function Nav() {
+  return (
+    <div className=" max-w-screen-5xl max-h-7  flex justify-evenly  space-x-7">
+      <div className="flex space-x-2 p-3">
+        <span className="w-3 h-3 bg-red-600 rounded-full text-red-600"></span>
+        <span className="w-3 h-3 bg-orange-600 rounded-full text-red-600"></span>
+        <span className="w-3 h-3 bg-green-600 rounded-full text-green-600"></span>
+        <p>icon</p>
+        <span>icon</span>
+      </div>
+
+      <span>icon</span>
+      <span className=" max-w-2xl h-5 border-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+      </span>
+
+      <span>icon</span>
+      <span>icon</span>
+      <span>ico</span>
+      <span>icon</span>
+    </div>
+  );
+}
+
+export default Nav;
