251e94e3019ff429e1097b9afb6a73684844c109
Sajid <contactme.qureshi@gmail.com>
2023-09-30 11:28:38 -0700

    removed bug on sidebar and reduce the code by using map method

---
+++ src/component/data.js
@@ -0,0 +1,50 @@
+const data = [{
+    id: 1,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+},
+{
+    id: 2,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+}, {
+    id: 3,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+}, {
+    id: 4,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+}, {
+    id: 5,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+}, {
+    id: 6,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+}, {
+    id: 7,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+},
+{
+    id: 8,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+},
+{
+    id: 9,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+},
+{
+    id: 10,
+    url: "http://www.w3.org/2000/svg",
+    text: 'Search'
+},]
+
+export default data
+
+
+
