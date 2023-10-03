3c1ddcdd9a0b9538b26c3b8e15f4dda35701bd13
Sajid <contactme.qureshi@gmail.com>
2023-10-03 10:11:42 -0700

    done with ui-design of sidebar just need to add datainit

--- src/component/sidebar.jsx
+++ src/component/sidebar.jsx
@@ -54,35 +54,50 @@ function Sidebar() {
     },
   ];
 
-  data.map(({ text, id, url }) => {
-    console.log(id);
-    console.log(text);
-    console.log(url);
-  });
+  const supp = [
+    {
+      id: 1,
+      url: "http://www.w3.org/2000/svg",
+      text: "Setting",
+    },
+    {
+      id: 2,
+      url: "http://www.w3.org/2000/svg",
+      text: "User-mangment",
+    },
+    {
+      id: 3,
+      url: "http://www.w3.org/2000/svg",
+      text: "Help & support",
+    },
+  ];
+
+  // data.map(({ text, id, url }) => {
+  //   console.log(id);
+  //   console.log(text);
+  //   console.log(url);
+  // });
   // console.log(data);
   return (
     <div>
-      <div className=" h-full p-3 w-60 dark:bg-gray-900 dark:text-gray-100">
-        <div>
-          <div className="flex items-center justify-between border-b-2 border-red-500">
-            <h2>Dashboard</h2>
-            <button className="p-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                className="w-5 h-5 fill-current dark:text-gray-100"
-              >
-                <rect width="352" height="32" x="80" y="96"></rect>
-                <rect width="352" height="32" x="80" y="240"></rect>
-                <rect width="352" height="32" x="80" y="384"></rect>
-              </svg>
-            </button>
-          </div>
-          <div
-            className="  border border-white 
-             "
-          >
-            <ul className=" pb-28 flex   leading-3 text-sm flex-col justify-between content-between     ">
+      <div className=" h-full p-3  w-52 dark:bg-gray-900 dark:text-gray-100 rounded-s-lg ">
+        <div className="flex items-center justify-between border-b-2 border-red-500">
+          <h2>Dashboard</h2>
+          <button className="p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              className="w-5 h-5 fill-current dark:text-gray-100"
+            >
+              <rect width="352" height="32" x="80" y="96"></rect>
+              <rect width="352" height="32" x="80" y="240"></rect>
+              <rect width="352" height="32" x="80" y="384"></rect>
+            </svg>
+          </button>
+        </div>
+        <div className=" flex flex-col justify-between   gap-y-44">
+          <div>
+            <ul className=" pb-28  leading-tight    ">
               {data.map(({ id, text, url }) => (
                 <li className="rounded-sm  " key={id}>
                   <a
@@ -102,11 +117,34 @@ function Sidebar() {
                 </li>
               ))}
             </ul>
-            <div className="flex items-center border-t-2 p-2 mt-12 space-x-4  bg-blue-400 ">
+          </div>
+
+          <div className=" ">
+            <ul className="   leading-tight    ">
+              {supp.map(({ id, text, url }) => (
+                <li className="rounded-sm  " key={id}>
+                  <a
+                    rel="noopener noreferrer"
+                    href="#"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <svg
+                      xmlns={url}
+                      viewBox="0 0 512 512"
+                      className="w-5 h-5 fill-current dark:text-gray-400"
+                    >
+                      <path d="M469.666,216.45,271.078,33.749a34,34,0,0,0-47.062.98L41.373,217.373,32,226.745V496H208V328h96V496H480V225.958ZM248.038,56.771c.282,0,.108.061-.013.18C247.9,56.832,247.756,56.771,248.038,56.771ZM448,464H336V328a32,32,0,0,0-32-32H208a32,32,0,0,0-32,32V464H64V240L248.038,57.356c.013-.012.014-.023.024-.035L448,240Z"></path>
+                    </svg>
+                    <span>{text}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <div className=" flex gap-2  text-center mt-2 border-t-2 p-2">
               <img
                 src="https://source.unsplash.com/100x100/?portrait"
                 alt=""
-                className="w-12 h-12 rounded-lg dark:bg-gray-500"
+                className="w-12 h-12  rounded-full dark:bg-gray-500"
               />
               <div>
                 <h2 className="text-lg font-semibold">Leroy Jenkins</h2>
@@ -129,3 +167,16 @@ function Sidebar() {
 }
 
 export default Sidebar;
+
+{
+  /* <div>
+
+<div
+  className="  border border-white 
+   "
+>
+ 
+  
+</div>
+</div> */
+}
